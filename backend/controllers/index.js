5cad3a9b6fe3bf0751588209aac62e276f3ff625
Novembri <Ridho.Novembri@mcdermott.com>
2025-06-10 19:26:11 +0700

    comments openai

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,39 +1,39 @@
-const OpenAI = require("openai");
+// const OpenAI = require("openai");
 const { Groq } = require("groq-sdk");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-exports.generateResponseOpenAI = async (req, res) => {
-  try {
-    const { prompt } = await req.body;
+// exports.generateResponseOpenAI = async (req, res) => {
+//   try {
+//     const { prompt } = await req.body;
 
-    // console.log('prompt', prompt)
+//     // console.log('prompt', prompt)
 
-    const openai = new OpenAI({
-      baseURL: "https://openrouter.ai/api/v1",
-      apiKey: process.env.OPENAI_API_KEY,
-    });
+//     const openai = new OpenAI({
+//       baseURL: "https://openrouter.ai/api/v1",
+//       apiKey: process.env.OPENAI_API_KEY,
+//     });
 
-    const response = await openai.chat.completions.create({
-      model: "meta-llama/llama-4-maverick:free",
-      // model: "meta-llama/llama-3.2-3b-instruct:free",
-      // model: "deepseek/deepseek-r1-0528:free",
-      // model: "google/gemini-2.0-flash-exp:free",
-      messages: [{ role: "user", content: prompt }],
-      temperature: 0.5,
-      stream: false,
-      temperature: 1,
-    });
+//     const response = await openai.chat.completions.create({
+//       model: "meta-llama/llama-4-maverick:free",
+//       // model: "meta-llama/llama-3.2-3b-instruct:free",
+//       // model: "deepseek/deepseek-r1-0528:free",
+//       // model: "google/gemini-2.0-flash-exp:free",
+//       messages: [{ role: "user", content: prompt }],
+//       temperature: 0.5,
+//       stream: false,
+//       temperature: 1,
+//     });
 
-    const script =
-      response.choices?.[0]?.message?.content || "No script generated";
-    res.json({ response: script });
-  } catch (error) {
-    console.log(error);
-    throw new Error(error);
-  }
-};
+//     const script =
+//       response.choices?.[0]?.message?.content || "No script generated";
+//     res.json({ response: script });
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error(error);
+//   }
+// };
 
 exports.generateResponseGroq = async (req, res) => {
   const { prompt } = await req.body;
