f993cc5d080a1480501a0d9643cce430d69b1862
Novembri <Ridho.Novembri@mcdermott.com>
2025-06-10 12:53:33 +0700

    add groq interface

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,9 +1,10 @@
 const OpenAI = require("openai");
+const { Groq } = require("groq-sdk");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-exports.generateResponse = async (req, res) => {
+exports.generateResponseOpenAI = async (req, res) => {
   try {
     const { prompt } = await req.body;
 
@@ -15,21 +16,53 @@ exports.generateResponse = async (req, res) => {
     });
 
     const response = await openai.chat.completions.create({
-      model: "meta-llama/llama-3.2-3b-instruct:free",
+      model: "meta-llama/llama-4-maverick:free",
+      // model: "meta-llama/llama-3.2-3b-instruct:free",
       // model: "deepseek/deepseek-r1-0528:free",
+      // model: "google/gemini-2.0-flash-exp:free",
       messages: [{ role: "user", content: prompt }],
       temperature: 0.5,
-      // stream: true,
+      stream: false,
       temperature: 1,
     });
 
     const script =
       response.choices?.[0]?.message?.content || "No script generated";
-
-    // res.json({ response: "Hi" });
     res.json({ response: script });
   } catch (error) {
     console.log(error);
-    throw new Error(error);    
+    throw new Error(error);
   }
+};
+
+exports.generateResponseGroq = async (req, res) => {
+  const { prompt } = await req.body;
+
+  // console.log(prompt);
+
+  const groq = new Groq();
+  const response = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    // model: "meta-llama/llama-4-scout-17b-16e-instruct",
+    model: "meta-llama/llama-4-maverick-17b-128e-instruct",
+    // model: "deepseek-r1-distill-llama-70b",
+    temperature: 1,
+    max_completion_tokens: 1024,
+    top_p: 1,
+    stream: false,
+    // response_format: {
+    //   type: "json_object",
+    // },
+    stop: null,
+  });
+
+  const script =
+    response.choices?.[0]?.message?.content || "No script generated";
+
+  res.json({ response: script });
 };
--- backend/routes/AppRoutes.js
+++ backend/routes/AppRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const route = express.Router()
 const ai = require('../controllers/index')
 
-route.post('/response', ai.generateResponse)
+// route.post('/response', ai.generateResponseOpenAI)
+route.post('/response', ai.generateResponseGroq)
 
 module.exports = route
