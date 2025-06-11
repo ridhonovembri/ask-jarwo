2f11f7df8327b1b614116ce75c5410143cdef6a5
Novembri <Ridho.Novembri@mcdermott.com>
2025-06-11 20:54:42 +0700

    remove .env

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export default axios.create({
-  // baseURL: 'http://localhost:3000/',
-  baseURL: 'https://ask-jarwo-be.vercel.app/',
+  baseURL: 'http://localhost:3000/',
+  // baseURL: 'https://ask-jarwo-be.vercel.app/',
   headers: {
     'Content-type': 'application/json'
   }
